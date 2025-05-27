0426b7dfaf94458e3bfdd5af8291961ba906a98c
shahalimshompod <shahalimsompod@gmail.com>
2025-05-27 16:00:14 +0600

    all server side setup is done now working on making some api end point

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// MIDDLE WARES
+app.use(cors());
+app.use(express.json());
+
+// database config
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ezm1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const allEvents = client.db("Eventora").collection("all-events");
+    const reviews = client.db("Eventora").collection("reviews");
+    const users = client.db("Eventora").collection("users");
+
+    // OPERATIONS
+    // GET OPERATIONS
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Eventora is on");
+});
+
+app.listen(port, () => {
+  console.log(`Eventora is open at ${port}`);
+});
